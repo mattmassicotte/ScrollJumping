43921dde76f9ea78ef56581f8017672e00d297cc
Matt Massicotte <85322+mattmassicotte@users.noreply.github.com>
2020-05-04 16:25:55 -0400

    Fix up setup, and include a workaround class

--- ScrollJumping/AppDelegate.swift
+++ ScrollJumping/AppDelegate.swift
@@ -24,7 +24,7 @@ class MyRulerView: NSRulerView {
         super.init(scrollView: scrollView, orientation: orientation)
 
         // to make the problem more obvious
-        ruleThickness = 100.0
+        ruleThickness = 77.0
     }
 
     required init(coder: NSCoder) {
@@ -32,10 +32,92 @@ class MyRulerView: NSRulerView {
     }
 }
 
+class JumpingBugWorkaroundScrollView: NSScrollView {
+    private var lastPosition: CGFloat = 0.0
+
+    private var textView: NSTextView? {
+        return documentView as? NSTextView
+    }
+
+    private var hasTextViewWithNonContiguousLayout: Bool {
+        return textView?.layoutManager?.hasNonContiguousLayout == true
+    }
+
+    private var workaroundChecksNeeded: Bool {
+        guard hasVerticalRuler else {
+            return false
+        }
+
+        guard hasTextViewWithNonContiguousLayout else {
+            return false
+        }
+
+        return true
+    }
+
+    // determined emperically and is possibly OS-version dependent. Unsure
+    // why the leading and trailing sides are the same
+    private var magicLeadingMarginDistance: CGFloat {
+        return 5.0
+    }
+
+    private var magicTrailingMarginDistance: CGFloat {
+        return 5.0
+    }
+
+    private var rulerSideTripLength: CGFloat {
+        let thickness = verticalRulerView?.requiredThickness ?? 0.0
+
+        return thickness - magicLeadingMarginDistance
+    }
+
+    private func shouldFilterScrollPoint(_ newPoint: NSPoint) -> Bool {
+        guard workaroundChecksNeeded else {
+            return false
+        }
+
+        let newPos = newPoint.x
+        let lastPos = lastPosition
+
+        // this is -1.0 and not 0.0 to give a little slack so that
+        // the scroll appears smooth on the trailing side
+        if lastPos < -1.0 && newPos == 0.0 {
+            return true
+        }
+
+        // this is a weird behavior on the trailing side
+        if lastPos == 0.0 && newPos == -magicTrailingMarginDistance {
+            return true
+        }
+
+        // handles the leading side cases
+        if lastPos < -rulerSideTripLength && newPos == -rulerSideTripLength {
+            return true
+        }
+
+        return false
+    }
+
+    override func scroll(_ clipView: NSClipView, to point: NSPoint) {
+        if shouldFilterScrollPoint(point) {
+            let filteredPoint = NSPoint(x: lastPosition, y: point.y)
+
+            super.scroll(clipView, to: filteredPoint)
+
+            return
+        }
+
+        lastPosition = point.x
+
+        super.scroll(clipView, to: point)
+    }
+}
+
 class ViewController: NSViewController {
     private let scrollView: NSScrollView
 
     init() {
+//        self.scrollView = JumpingBugWorkaroundScrollView(frame: NSRect(x: 0, y: 0, width: 600.0, height: 300.0))
         self.scrollView = NSScrollView(frame: NSRect(x: 0, y: 0, width: 600.0, height: 300.0))
 
         super.init(nibName: nil, bundle: nil)
@@ -51,14 +133,20 @@ class ViewController: NSViewController {
         scrollView.verticalRulerView = MyRulerView(scrollView: scrollView, orientation: .verticalRuler)
         scrollView.rulersVisible = true
 
+        // basic textview configuration
         let textView = NSTextView(frame: NSRect(x: 0, y: 0, width: 600.0, height: 300.0))
 
         textView.insertionPointColor = NSColor.textColor
         textView.textColor = NSColor.textColor
         textView.layoutManager?.allowsNonContiguousLayout = true
         textView.isEditable = true
+
+        // ensure text is not wrapped
+        let max = CGFloat.greatestFiniteMagnitude
+        textView.textContainer?.size = CGSize(width: max, height: max)
         textView.isVerticallyResizable = true
-        textView.isHorizontallyResizable = false
+        textView.isHorizontallyResizable = true
+        textView.textContainer?.widthTracksTextView = false
 
         scrollView.documentView = textView
 
